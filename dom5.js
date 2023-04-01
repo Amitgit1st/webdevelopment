7a5a369e4c6341357aa2b100a6558d17c11d2a16
Amitgit1st <aamitp6297@gmail.com>
2023-04-01 22:44:17 +0530

    addingAttribute

---
+++ dom5.js
@@ -0,0 +1,5 @@
+// SETTING ATTRIBUTE TO NEWLY CREATED ELEMENT
+
+var newDiv=document.createElement('div');   //NEW ELEMENT div IS CREATED
+newDiv.setAttribute('title','NEW ATTRIBUTE'); //TITLE ATTRIBUTE IS CREATED WHOSE VALUE IS NEW ATTRIBUTE 
+console.log(newDiv);
