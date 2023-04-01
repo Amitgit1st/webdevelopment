7863f3787d33d644369b6ff759c1d3ff1bd75c9e
Amitgit1st <aamitp6297@gmail.com>
2023-04-01 22:40:17 +0530

    lastElementChild

---
+++ dom3.js
@@ -0,0 +1,8 @@
+// TO GRAB id='items'
+var itemList = document.querySelector('#items');
+
+//lastElementChild
+console.log(itemList.lastElementChild);
+
+//lastElement color changes to red
+itemList.lastElementChild.style.color = 'red';
